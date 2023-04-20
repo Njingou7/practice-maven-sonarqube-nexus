566d2d51db54302c617b6101152e03de18602eaa
Njingou7 <70728931+Njingou7@users.noreply.github.com>
2023-04-20 12:41:32 -0400

    modified Appcode by Apo

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String name = "ApoNjingou";
+        System.out.println(name);
 	}
 
 }
